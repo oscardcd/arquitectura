6c43c8bdee8419684e71bccec432e15f43f31953
oscardcd <oscardcd17@gmail.com>
2020-03-04 18:04:05 -0500

    primer commit

---
+++ arquitectura_code/nodejs/index.js
@@ -0,0 +1,17 @@
+'use strict'
+//npm install nodemon --save-dev
+//npm install express --save
+
+//declaracion de librerias a usar en el servidor:
+const express =  require('express');
+//definimos el uso de app como interprete de express
+const app=express();
+//declaramos el puerto del servicio:
+const port = 3800;
+
+//mensaje de bienvenida desde el servidor:
+app.listen(port, ()=>{
+    console.log('bienvenidos a mi servidor Nodejs');
+    console.log('puedes ingresar a la url: localhost:3800//');
+  
+});
